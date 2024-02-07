21bf5623426732ac4b54ff7b63d28cd772bf13ac
chiragksharma <chiragksharma15@gmail.com>
2024-02-07 16:10:58 +0530

    fixing the port

--- api/app.py
+++ api/app.py
@@ -80,4 +80,4 @@ async def root():
     return responses.RedirectResponse(url="/docs")
 
 if __name__ == '__main__':
-    uvicorn.run("main:app",port=5000,host="0.0.0.0",log_level="info")
+    uvicorn.run("main:app",port=os.getenv("PORT", default=5000),host="0.0.0.0",log_level="info")
